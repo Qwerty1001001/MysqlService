f04490a5ac96944bdba4dee0314c0f4d5292eb42
Qwerty1001001 <52752477+Qwerty1001001@users.noreply.github.com>
2019-07-10 11:08:19 -0500

    Bat Temp

---
+++ MySQL-5.7.21-winx64.bat
@@ -0,0 +1,4 @@
+@echo off
+SET PATH=%PATH%;D:\mysql\MySQL-5.7.21-winx64\bin;
+echo Servidor Iniciado en D:\mysql\MySQL-5.7.21-winx64 en el Puerto 3321 Versión 5.7.21
+D:\mysql\MySQL-5.7.21-winx64\bin\mysqld.exe --defaults-file=D:\mysql\MySQL-5.7.21-winx64\MySQL.ini
